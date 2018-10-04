61a58cc8b7bba48a6a72c07332b642d560ea2add
Huangtiangang <huangtiangang@sunlands.com>
2018-10-04 17:42:04 +0800

    update

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,3 +1,3 @@
-#!/bin bash
-docker build -t chat-room:latest .
+#!/bin/sh
+docker build -t chat-room:latest ../
 docker run --rm --name chat-room -p 8001:8080 -v /var/jenkins_home/logs:/var/log chat-room:latest
